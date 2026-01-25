ae9d34b94d70bdb99425e23698923e6d8645ae54
thisismegopi <gopinath10007@gmail.com>
2026-01-25 15:15:14 +0530

    tuari setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import path from 'path';
 import { defineConfig } from 'vite';
+import path from 'path';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
@@ -9,5 +9,8 @@ export default defineConfig({
         alias: {
             '@': path.resolve(__dirname, './src'),
         },
+    },
+    server: {
+        port: 3000,
     },
 });
